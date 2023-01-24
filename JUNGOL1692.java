82c652d5edc573896d8133fdb6a5fd9aa672841c
githyuniiee <109260733+githyuniiee@users.noreply.github.com>
2023-01-24 10:49:33 +0900

    Update JUNGOL1692.java
    JUGNOL 1692 : 곱셈

--- JUNGOL1692.java
+++ JUNGOL1692.java
@@ -1,21 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class JUNGOL1692 {
 
-	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
-		int a = keyboard.nextInt();
-		int b = keyboard.nextInt();
-		int b1 = b%10;
-		int b2 = (b/10)%10;
-		int b3 = b/100;
+		int N1 = Integer.parseInt(br.readLine());
+		int N2 = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		int temp = N2%10;
+		sb.append(N1*temp + "\n");
+	
+		temp = (N2/10)%10;
+		sb.append(N1*temp + "\n");
+		
+		temp = (N2/100);
+		sb.append(N1*temp + "\n");
 		
 		
-		System.out.println(a*b1);
-		System.out.println(a*b2);
-		System.out.println(a*b3);
-		System.out.println(a*b);
+		sb.append(N1*N2);
+		System.out.println(sb);
 	}
 	
 }
